cbe73dc403f8290aa44339b824978fd264e538df
saravananvimal21 <saravanan211118@gmail.com>
2025-11-29 19:25:46 +0530

    created login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './component/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl p-5 font-bold underline">
-        Hello world!
-      </h1>
+      <Login />
     </>
   )
 }
